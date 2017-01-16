6992d8682ad37552c09c6681f34ac539b231bb0c
Axel <lezinaxel@gmail.com>
2017-01-16 15:24:06 +0100

    Affichage de la string en gras

--- index.php
+++ index.php
@@ -9,7 +9,12 @@ class StrUtils {
 	public function toString(){
 		return $this -> _str;
 	}
+	public function bold(){
+		$this -> _str ='<strong>' . $this -> _str . '</strong>';
+		return $this -> _str;
+	}
 }
 
 $maPhrase = new StrUtils("J'aime Michèle");
 echo $maPhrase -> toString();
+echo $maPhrase -> bold();
